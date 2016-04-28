27fb76c59f5dc6ed2eb011c8662ec76ec181abfc
server.git <server.git@server>
2016-04-28 09:19:39 +0800

    add some notes

--- check-for-iourc.sh
+++ check-for-iourc.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
-
-#开机自动检查iourc文件是否正确，若不正确，重新生成iourc
-
+#2016-04-28
+#check the iourc file, if the iourc do not contain a correct hostname, use keygen to make a new iourc
 host=`hostname`
 iourc=`cat /opt/iou/bin/iourc |awk 'NR>1'|cut -d "=" -f 1`
-if [ $iourc != $host ]
+if [ "$iourc" != "$host" ]
 then
 	echo "iourc is not ok, making a new iou license..."
 	python /opt/iou/bin/CiscoIOUKeygen.py | sed -n '7,8p'>/opt/iou/bin/iourc
